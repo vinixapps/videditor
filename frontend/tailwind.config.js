6efa66cad16ef874640f95c5c2ee2b9d8b7acd62
vinixapps <vinixemails@gmail.com>
2025-09-29 13:24:28 +0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,14 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}", // kalau pakai Next.js app dir
   ],
-  theme: {
-    extend: {
-      // Tambahkan custom color/font jika perlu
-      // colors: { primary: "#..." }
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
-};
-
+}
